6cdfc96aa467a63427472e7883a94fa59d62c9c2
Ruble <a2654350229@outlook.com>
2020-11-12 22:55:07 +0800

    daffodil

---
+++ 水仙花数（daffodil）/水仙花数（daffodil）.cpp
@@ -0,0 +1,12 @@
+﻿#include<stdio.h>
+const int max = 999;
+int main()
+{
+	for (int min = 100; min <= max; min++) {
+		int a = min / 100;//�ٷ�λ
+		int b = (min % 100) / 10;//ʮ��λ
+		int c = min % 10;//����λ
+		if ((a * a * a) + (b * b * b) + (c * c * c))printf("%d\n", min);
+	}
+	return 0;
+}
